772695b19831485e0c9eec60ca9d20f337391299
William Ryan O'Sullivan <wrosullivan88@gmail.com>
2024-02-14 16:04:56 -0600

    testtt

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
 
+  #testing...
   
   resources :articles do
     resources :comments #nested resource
